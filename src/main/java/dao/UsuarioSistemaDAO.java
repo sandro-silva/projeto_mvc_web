423684ec627564880388f973b149f37a35cfce7d
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-02-19 00:55:50 -0300

    Implementação UsuarioDAO

---
+++ src/main/java/dao/UsuarioSistemaDAO.java
@@ -0,0 +1,40 @@
+package dao;
+
+import model.UsuarioSistema;
+import util.Conexao;
+
+import java.sql.*;
+
+public class UsuarioSistemaDAO {
+
+    public UsuarioSistema autenticar(String login, String senha) {
+
+        String sql = "SELECT * FROM usuarios_sistema WHERE login = ? AND senha = ?";
+
+        try (
+            Connection conn = Conexao.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)
+        ) {
+
+            ps.setString(1, login);
+            ps.setString(2, senha);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                UsuarioSistema u = new UsuarioSistema();
+                u.setId(rs.getLong("id"));
+                u.setLogin(rs.getString("login"));
+                u.setSenha(rs.getString("senha"));
+
+                return u;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
