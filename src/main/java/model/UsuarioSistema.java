29821c70d3fce5be0f29b7fa1c4da258158aa1f6
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-02-19 22:49:47 -0300

    Implementação UsuarioSistema

--- src/main/java/model/UsuarioSistema.java
+++ src/main/java/model/UsuarioSistema.java
@@ -5,6 +5,7 @@ public class UsuarioSistema {
     private Long id;
     private String login;
     private String senha;
+    private String perfil;
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -14,4 +15,9 @@ public class UsuarioSistema {
 
     public String getSenha() { return senha; }
     public void setSenha(String senha) { this.senha = senha; }
+
+    public String getPerfil() { return perfil; }
+
+    public void setPerfil(String perfil) { this.perfil = perfil; }
+    
 }
