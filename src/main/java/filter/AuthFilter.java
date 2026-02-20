7b43cb3c99ff1c3c87aa0cddc3c0fbfd260ed1a5
sandro.silva1 <68612228+sandro-silva@users.noreply.github.com>
2026-02-19 22:52:19 -0300

    Implementação AuthFilter

--- src/main/java/filter/AuthFilter.java
+++ src/main/java/filter/AuthFilter.java
@@ -3,12 +3,12 @@ package filter;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.*;
-
 import java.io.IOException;
 
-@WebFilter({"/dashboard", "/usuario"})
+@WebFilter("/*")
 public class AuthFilter implements Filter {
 
+    @Override
     public void doFilter(
             ServletRequest request,
             ServletResponse response,
@@ -18,12 +18,28 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
 
+        String uri = req.getRequestURI();
+
+        // Páginas públicas
+        if (uri.contains("login") || uri.contains("css") || uri.contains("js")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         HttpSession session = req.getSession(false);
 
         if (session == null ||
             session.getAttribute("usuarioLogado") == null) {
 
             resp.sendRedirect("login");
+            return;
+        }
+
+        String perfil = (String) session.getAttribute("perfil");
+
+        // 🔒 Apenas ADMIN pode acessar /usuario
+        if (uri.contains("/usuario") && !"ADMIN".equals(perfil)) {
+            resp.sendRedirect("dashboard");
             return;
         }
 
